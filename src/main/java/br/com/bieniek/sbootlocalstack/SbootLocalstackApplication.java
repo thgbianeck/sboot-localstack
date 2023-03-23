50c1b20c811c36b59c587deb7562f682fdaac9eb
Bianeck <thiagobianeck@gmail.com>
2023-03-22 22:33:57 -0300

    Initial commit

---
+++ src/main/java/br/com/bieniek/sbootlocalstack/SbootLocalstackApplication.java
@@ -0,0 +1,13 @@
+package br.com.bieniek.sbootlocalstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbootLocalstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbootLocalstackApplication.class, args);
+	}
+
+}
